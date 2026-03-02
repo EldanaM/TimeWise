a28be3fce0d994030191f370247b302609f6ec52
Eldana_M <moldobayevae@gmail.com>
2026-03-02 21:20:55 +0500

    Add task admin

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Task, TimeLog, Notification
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'created_at']
+    list_filter = ['user']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'user', 'estimated_time', 'is_completed']
+    list_filter = ['is_completed', 'category', 'user']
+
+@admin.register(TimeLog)
+class TimeLogAdmin(admin.ModelAdmin):
+    list_display = ['task', 'actual_time', 'user', 'created_at']
+    list_filter = ['user']
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'type', 'is_read', 'created_at']
+    list_filter = ['type', 'is_read', 'user']
