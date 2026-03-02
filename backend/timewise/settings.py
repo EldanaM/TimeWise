887392f976049bc10de3f198756298103ce4031d
Eldana_M <moldobayevae@gmail.com>
2026-03-02 21:12:00 +0500

    Change settings

--- backend/timewise/settings.py
+++ backend/timewise/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'users',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
